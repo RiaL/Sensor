907e271b78da0ba0eab0701693a6178bfc32795f
kkutt <krzysztof.kutt@gmail.com>
2012-11-05 18:36:18 +0100

    sensory w bashu

---
+++ Sensor/sensor2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+##########################################################
+# Config
+ 
+SERVER="localhost"
+PORT=8002
+RESOURCEID="myComputer"
+METRIC="cpuUsage"
+ 
+##########################################################
+# Main
+ 
+exec 3<>/dev/tcp/${SERVER}/${PORT}
+
+#nieskonczona petla
+while [ 1 -le 10 ] ; do
+	#wylosuj wartosc
+	let VALUE=$RANDOM%100
+
+	#wyslij dane na port
+	echo -e "${RESOURCEID}:${METRIC}:${VALUE}" >&3
+	#pokaz dane w konsoli
+	echo -e "${RESOURCEID}:${METRIC}:${VALUE}"
+
+	#poczekaj losowy czas (w sekundach)
+	let TIME=$RANDOM%8
+	sleep $TIME
+done
+
+
